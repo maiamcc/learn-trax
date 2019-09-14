363a8d7174358ac8c42a6c1038752026767a222a
Maia <maia.mcc@gmail.com>
2019-09-13 23:32:02 -0400

    let's see if this works

--- learn-trax.py
+++ learn-trax.py
@@ -15,6 +15,7 @@
 # >>> basstrack[5] = basstrack[5].copy(value=120)
 
 import argparse
+from copy import deepcopy
 from typing import List
 
 from mido import MidiFile
@@ -23,6 +24,12 @@ from mido.messages.messages import Message
 
 VOL_CONTROL = 7
 PAN_CONTROL = 10
+
+VOL_BACKGROUND = 55
+VOL_FOREGROUND = 120
+
+PAN_BACKGROUND = 56
+PAN_FOREGROUND = 76
 
 
 def argument_parser():
@@ -44,8 +51,44 @@ def is_vol_control_or_pan_control(msg: Message) -> bool:
     return msg.type == 'control_change' and msg.control in [VOL_CONTROL, PAN_CONTROL]
 
 
-def foreground_track_at_index(tracks: List[MidiTrack], i: int) -> List[MidiTrack]:
-    pass
+def vol_background() -> Message:
+    return Message('control_change', control=VOL_CONTROL, value=VOL_BACKGROUND)
+
+
+def vol_foreground() -> Message:
+    return Message('control_change', control=VOL_CONTROL, value=VOL_FOREGROUND)
+
+
+def pan_background() -> Message:
+    return Message('control_change', control=PAN_CONTROL, value=PAN_BACKGROUND)
+
+
+def pan_foreground() -> Message:
+    return Message('control_change', control=PAN_CONTROL, value=PAN_FOREGROUND)
+
+
+def foreground(track: MidiTrack) -> MidiTrack:
+    ret = deepcopy(track)  # can i use track.copy() for this?
+    ret.insert(1, vol_foreground())
+    ret.insert(1, pan_foreground())
+    return ret
+
+
+def background(track: MidiTrack) -> MidiTrack:
+    ret = deepcopy(track)  # can i use track.copy() for this?
+    ret.insert(1, vol_background())
+    ret.insert(1, pan_background())
+    return ret
+
+
+def foreground_track_at_index(tracks: List[MidiTrack], index: int) -> List[MidiTrack]:
+    ret = []
+    for i, trk in enumerate(tracks):
+        if i == index:
+            ret.append(foreground(trk))
+        else:
+            ret.append(background(trk))
+    return ret
 
 
 if __name__ == '__main__':
@@ -57,22 +100,10 @@ if __name__ == '__main__':
     # TODO: make all tracks same instrument?
 
     # idk how consistent this is but sib-generated midis, first track is just metadata
-    tracks = mid.tracks[1:]
+    tracks = [rm_volume_and_pan_set(t) for t in mid.tracks[1:]]
 
+    alto_tracks = foreground_track_at_index(tracks, 2)
 
-"""
-instrument_tracks = [t for t in mid.tracks if not_meta(t)]
-for t in instrument_tracks:
-    remove_vol_control(t)
-    remote_pan_control(t)
-
-foreground_index = 2  # TODO: one midi object per track-to-foreground
-for i, t in enumerate(instrument_tracks):
-    if i == foreground_index:
-        make_foreground(t)
-        continue
-    make_background(t)
-
-# TODO: convert to mp3
-mid.save('out.mid')
-"""
+    outfile = MidiFile()
+    outfile.tracks = alto_tracks
+    outfile.save('alto.mid')
