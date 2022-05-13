e73e0d6a093a3c9ab560d0897c871c1f22b08572
Maia <maia.mcc@gmail.com>
2022-05-12 20:42:17 -0400

    can derive voices ffrom tracks

--- learn-trax.py
+++ learn-trax.py
@@ -16,7 +16,7 @@ def argument_parser():
         help='path to base midi file'
     )
     parser.add_argument(
-        'voice_parts',
+        '--voices', '-v',
         type=str,
         help='comma-separated list of voice parts to be used for naming files: fair-phyllis-<sop>.wav'
     )
@@ -33,8 +33,6 @@ if __name__ == '__main__':
     parser = argument_parser()
     args = parser.parse_args()
 
-    voice_parts = args.voice_parts.split(',')
-
     mid = MidiFile(args.input_mid)
 
     # TODO: make all tracks same instrument?
@@ -44,9 +42,14 @@ if __name__ == '__main__':
 
     # idk how consistent this is but sib-generated midis, first track is just metadata
     meta_track, voice_tracks = utils.cleaned_meta_and_voice_tracks(mid)
+    voices = args.voices.split(',') if args.voices else [trk.name.lower() for trk in voice_tracks]
+    if len(voices) != len(voice_tracks):
+        raise ValueError("Invalid number of voices passed, does not align with number of tracks!"
+            "\n\tVoices passed: {}\n\tTrack names: {}".
+            format(voices, [trk.name.lower() for trk in voice_tracks]))
 
     for i in range(len(voice_tracks)):
         # TODO: derive filename (and path?) from infile
-        base_filename = os.path.join(directory, '{}-{}'.format(prefix, voice_parts[i]))
+        base_filename = os.path.join(directory, '{}-{}'.format(prefix, voices[i]))
         print('making practice track {} at index: {}'.format(base_filename, i))
         utils.practice_track_at_index(mid.ticks_per_beat, voice_tracks, meta_track.copy(), base_filename, i)
