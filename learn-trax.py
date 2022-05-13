956ad78f716a49e51365ea5b27a985dfa372f1e4
Maia <maia.mcc@gmail.com>
2022-05-12 20:32:01 -0400

    inherit directory and filename

--- learn-trax.py
+++ learn-trax.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import argparse
+import os.path
 
 from mido import MidiFile
 
@@ -15,14 +16,14 @@ def argument_parser():
         help='path to base midi file'
     )
     parser.add_argument(
-        'file_prefix',
-        type=str,
-        help='prefix for outfiles: <prefix>-alto.mid'
-    )
-    parser.add_argument(
         'voice_parts',
         type=str,
         help='comma-separated list of voice parts to be used for naming files: fair-phyllis-<sop>.wav'
+    )
+    parser.add_argument(
+        '--prefix', '-p',
+        type=str,
+        help='prefix for outfiles: <prefix>-alto.mid'
     )
 
     return parser
@@ -38,11 +39,14 @@ if __name__ == '__main__':
 
     # TODO: make all tracks same instrument?
 
+    directory, filename = os.path.split(args.input_mid)
+    prefix = args.prefix if args.prefix else os.path.splitext(filename)[0]
+
     # idk how consistent this is but sib-generated midis, first track is just metadata
     meta_track, voice_tracks = utils.cleaned_meta_and_voice_tracks(mid)
 
     for i in range(len(voice_tracks)):
         # TODO: derive filename (and path?) from infile
-        base_filename = '{}-{}'.format(args.file_prefix, voice_parts[i])
+        base_filename = os.path.join(directory, '{}-{}'.format(prefix, voice_parts[i]))
         print('making practice track {} at index: {}'.format(base_filename, i))
         utils.practice_track_at_index(mid.ticks_per_beat, voice_tracks, meta_track.copy(), base_filename, i)
