15be511cfcaa14fe4ab52351de4a0646728edd6f
Maia <maia.mcc@gmail.com>
2022-09-16 20:39:21 -0400

    select-traxx outputs in same dir as input midi by default

--- select-trax.py
+++ select-trax.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import argparse
+import os
 
 from mido import MidiFile
 
@@ -23,6 +24,11 @@ def argument_parser():
         'track_names',
         type=str,
         help='comma-separated list of track names to foreground (case-insensitive)'
+    )
+    parser.add_argument(
+        '--output_dir', '-o',
+        type=str,
+        help='directory in which to output tracks (may be absolute, or relative to directory of input_mid). By default, tracks are saved to the same directory as input_mid.'
     )
 
     return parser
@@ -48,4 +54,16 @@ if __name__ == '__main__':
                             format(req, ", ".join(names)))
         indices.append(i)
 
-    utils.practice_track_with_foregrounds(mid.ticks_per_beat, voice_tracks, meta_track, args.outfile, indices)
+
+    directory, filename = os.path.split(args.input_mid)
+    output_dir = args.output_dir
+    if not output_dir:
+        # by default, output files to the same directory as the input mid
+        output_dir = directory
+    elif not os.path.isabs(output_dir):
+        # if output_dir isn't an absolute path, it's assumed relative to directory of the input mid
+        output_dir = os.path.join(directory, output_dir)
+    # otherwise, it's just an absolute path
+
+    outfile_path = os.path.join(output_dir, args.outfile)
+    utils.practice_track_with_foregrounds(mid.ticks_per_beat, voice_tracks, meta_track, outfile_path, indices)
